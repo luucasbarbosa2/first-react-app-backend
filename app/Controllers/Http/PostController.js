152eafd3d20eefdda57f4457ed8849d0e3a7c0f5
lucasbarbosa <lucasbarbosa@IFSPSAOCARLOS.EDU.BR>
2018-12-20 19:09:40 -0200

    first commit

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+
+const Post = use('App/Models/Post')
+
+class PostController {
+ 
+
+  async index ({ request, response, view }) {
+    const posts = await Post.all()
+    return posts
+  }
+
+  async store ({ request, response }) {
+    const data = request.only(['content'])
+    const post = await Post.create(data)
+    return post
+  }
+
+
+  async show ({ params, request, response, view }) {
+    const post = await Post.find(params.id)
+    return post
+  }
+
+  async update ({ params, request, response }) {
+    const data = request.only(['content'])
+    const post = await Post.find(params.id)
+
+    await post.save()
+    return post
+  }
+
+  async destroy ({ params, request, response }) {
+    const post  = Post.find(params.id)
+    await post.delete()
+  }
+}
+
+module.exports = PostController
